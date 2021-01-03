5f871ab3f50992addad5615b28ac766eb877263c
sravan <myselfsravanjr@gmail.com>
2021-01-03 15:13:28 +0530

    digital live clock

--- src/App.js
+++ src/App.js
@@ -4,20 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="clock">
+        <h2>The Time is :</h2>
+        <div id="time">
+          <div><span id="hour">00</span><span>Hours</span></div>
+          <div><span id="minutes">00</span><span>Minutes</span></div>
+          <div><span id="seconds">00</span><span>Seconds</span></div>
+          <div><span id="ampm">AM</span></div>
+        </div>
+      </div>
     </div>
   );
 }
